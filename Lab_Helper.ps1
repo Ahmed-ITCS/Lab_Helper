122b7d5be9ee14b94e348b80e8fa65f59d986eb4
Ahmed-ITCS <59608481+Ahmed-ITCS@users.noreply.github.com>
2021-07-26 16:27:46 +0500

    Update Lab_Helper.ps1

--- Lab_Helper.ps1
+++ Lab_Helper.ps1
@@ -2,15 +2,15 @@ $choice = Read-Host "enter 1 to make cpp file\nenter 2 to make ppt\nenter 3 to m
 if($choice -eq 1){
 	notepad programgenerated.cpp
 }
-else if($x -eq 2){
+if($x -eq 2){
    notepad programgenerated.ppt
 } 
-else if($x -eq 3){
+if($x -eq 3){
    notepad programgenerated.docx
 }
-elseif($x -eq 4){
+if($x -eq 4){
    shutdown -L -t 1
 } 
-elseif($x -eq 5){
+if($x -eq 5){
     shutdown -r -t 1
 }
